754583c79ac5259aed310a69e56a1b7ca3c2bfc0
Diego Zapata <diego.zapata@roiback.com>
2025-01-17 06:54:09 -0500

    ajuste newsletter

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -5,10 +5,10 @@ import styles from './styles.module.scss'
 const Newsletter = () => {
   return (
     <div className={styles.section}>
-      <h3 className={styles.title}>Subscribe Newsletters</h3>
+      <h3 className={styles.title}>Suscríbete al Newsletters</h3>
       <form action="" className={styles.form}>
-        <input type="email" className={styles.input} required placeholder='Enter your email' />
-        <button className={styles.btn}>Subcribe Now</button>
+        <input type="email" className={styles.input} required placeholder='Ingresa tu email' />
+        <button className={styles.btn}>Suscríbete</button>
       </form>
     </div>
   )
